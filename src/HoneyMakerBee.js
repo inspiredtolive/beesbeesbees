0df58417a28208ab0c4ed4e8cc66aeb0d41f58cd
Marco Chan <inspiredtolive0@gmail.com>
2016-08-01 12:22:50 -0700

    Implement HoneyMakerBee

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,16 @@
 var HoneyMakerBee = function() {
+  Bee.call(this);
+  this.age = 10;
+  this.job = 'make honey';
+  this.honeyPot = 0;
 };
 
+_.extend(HoneyMakerBee.prototype, Bee.prototype);
+
+HoneyMakerBee.prototype.makeHoney = function () {
+  this.honeyPot++;
+};
+
+HoneyMakerBee.prototype.giveHoney = function () {
+  this.honeyPot--;
+};
