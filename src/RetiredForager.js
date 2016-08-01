2083e68d1bc9c780d1f84956d425c845b4d0b878
Marco Chan <inspiredtolive0@gmail.com>
2016-08-01 12:23:32 -0700

    Implement RetiredForager

--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,3 +1,17 @@
 var RetiredForagerBee = function() {
+  ForagerBee.call(this);
+  this.age = 40;
+  this.job = 'gamble';
+  this.canFly = false;
+  this.color = 'grey';
 };
 
+_.extend(RetiredForagerBee.prototype, ForagerBee.prototype);
+
+RetiredForagerBee.prototype.forage = function () {
+  return 'I am too old, let me play cards instead';
+};
+
+RetiredForagerBee.prototype.gamble = function () {
+  this.treasureChest.push('gold');
+};
